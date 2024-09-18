d805dd48e58517c57047e19b65187b41f0b6230e
LunaGarcia <luna_penagos82221@elpoli.edu.co>
2024-09-18 02:37:08 -0500

    Manager register
    Screen for the completion of the registration with the manager role

--- Script/registerTeacher.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector('#btn');
-let sidebar = document.querySelector('.sidebar');
-let container = document.querySelector('.container');
-
-btn.onclick = function()
-{
-    sidebar.classList.toggle('active');
-    container.classList.toggle('active');
-};
